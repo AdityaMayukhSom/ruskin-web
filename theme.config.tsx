090cc1b1f5ea21bafc157d575ab4345a53c55599
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-11 10:56:55 +0530

    ADD: nextra theme for docs

---
+++ theme.config.tsx
@@ -0,0 +1,11 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const nextraThemeConfig: DocsThemeConfig = {
+    logo: <span>devstream / ruskin</span>,
+    project: {
+        link: 'https://github.com/adityamayukhsom/ruskin'
+    }
+    // ... other theme options
+}
+
+export default nextraThemeConfig
