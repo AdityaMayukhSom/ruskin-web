d57d8a2a962b3e58f77cad0a2c966ccd8bff4fe0
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-15 23:30:44 +0530

    ADD: production decription section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
 import { Navbar } from "@/components/Navbar";
+import ProductionDescription from "@/components/ProductDescription";
 export default function Home() {
   return (
     <main className="font-circular-std bg-primary-background-color text-primary-font-color">
       <Navbar />
       <Hero />
+      <ProductionDescription />
     </main>
   );
 }
