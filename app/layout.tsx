6c2dc83679cbc70bc74bd73ddf1691ebfdfcb10c
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-15 17:46:01 +0530

    FIX: font import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { circularStd } from "@/fonts";
-
+import { circularStd } from "@/styles/fonts";
 
 export const metadata: Metadata = {
   title: "Ruskin - Devstream",
