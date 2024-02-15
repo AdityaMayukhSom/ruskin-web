8ce6ce08ba70eb0b73d2a2884953f38e21809870
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-15 23:30:54 +0530

    ADD: production description component

---
+++ components/ProductDescription.tsx
@@ -0,0 +1,20 @@
+export default function ProductionDescription() {
+  return (
+    <section className="grid grid-cols-2 w-full px-32 py-32">
+      <div className="">
+        <h1 className="text-5xl font-bold py-2">Your Product</h1>
+        <h1 className="text-5xl font-bold py-2">Your Language</h1>
+        <h1 className="text-5xl font-bold py-2">Your Platform</h1>
+
+        <p className=" py-6 text-2xl pr-20">
+          Packing the power of robust code hosting, seamless collaboration
+          through code reviews, and flexible pipelines for any language. All
+          with the security and governance policies developers need to write and
+          deliver code with confidence.
+        </p>
+      </div>
+
+      <div></div>
+    </section>
+  );
+}
