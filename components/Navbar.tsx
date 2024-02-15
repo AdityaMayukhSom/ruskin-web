fc6916fa9485f380d9e48125bf244d0276aa583b
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-15 17:46:10 +0530

    ADD: navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import "@/styles/components/Navbar.module.css";
+// import devstreamLogo from '@/public/devstream.svg'
+
+export function Navbar() {
+  return (
+    <nav className="flex flex-row items-center justify-between px-10 py-8">
+      <span className="flex flex-row items-center gap-x-2">
+        <Image
+          alt="devstream logo"
+          height={32}
+          width={32}
+          src="/devstream.svg"
+        />
+        <span>
+          <Link href="https://www.devstream.in/">devstream</Link>
+          &nbsp;&#47;&nbsp;
+          <Link href="https://ruskin.devstream.in/">ruskin</Link>
+        </span>
+      </span>
+      <ul className="flex flex-row gap-x-8">
+        <li>
+          <Link href="/faq">FAQ</Link>
+        </li>
+        <li>
+          <Link href="/docs">Documentation</Link>
+        </li>
+        <li>
+          <Link href="https://www.github.com/AdityaMayukhSom/ruskin">
+            GitHub
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
