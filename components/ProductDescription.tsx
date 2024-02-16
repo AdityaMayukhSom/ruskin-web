d1106d6d8a8897c787cdc61e2143198c05ce6580
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-16 09:31:14 +0530

    ADD: product description drop shadow and image

--- components/ProductDescription.tsx
+++ components/ProductDescription.tsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
+
 export default function ProductionDescription() {
   return (
-    <section className="grid grid-cols-2 w-full px-32 py-32">
+    <section className="relative w-full grid grid-cols-2 justify-center items-center px-32 py-32 max-h-min">
       <div className="">
-        <h1 className="text-5xl font-bold py-2">Your Product</h1>
-        <h1 className="text-5xl font-bold py-2">Your Language</h1>
-        <h1 className="text-5xl font-bold py-2">Your Platform</h1>
+        <h1 className="text-5xl font-bold py-2 drop-shadow-2xl">
+          Your Product
+        </h1>
+        <h1 className="text-5xl font-bold py-2 drop-shadow-2xl">
+          Your Language
+        </h1>
+        <h1 className="text-5xl font-bold py-2 drop-shadow-2xl">
+          Your Platform
+        </h1>
 
-        <p className=" py-6 text-2xl pr-20">
+        <p className=" py-6 text-2xl pr-32 leading-8 drop-shadow-2xl">
           Packing the power of robust code hosting, seamless collaboration
           through code reviews, and flexible pipelines for any language. All
           with the security and governance policies developers need to write and
@@ -14,7 +22,16 @@ export default function ProductionDescription() {
         </p>
       </div>
 
-      <div></div>
+      <div className="relative w-full h-120 flex justify-center items-center">
+        <Image
+          src="/product-description.png"
+          alt="ruskin is packed with power, check how"
+          width="20"
+          height="20"
+          sizes="100vw"
+          className="h-full w-auto object-cover"
+        />
+      </div>
     </section>
   );
 }
