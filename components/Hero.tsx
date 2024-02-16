be66415672a4b64bfa0c25ca9532e6006728d124
Aditya Mayukh Som <adi.kg2@gmail.com>
2024-02-16 09:30:39 +0530

    FIX: css update in hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <section className="w-full flex flex-col justify-center items-center pt-32">
-      <h1 className=" text-3xl md:text-5xl font-semibold py-2">
+      <h1 className=" text-3xl md:text-5xl font-semibold py-2 drop-shadow-2xl">
         Open Source Real Time Distributed
       </h1>
-      <h1 className=" text-3xl md:text-5xl font-semibold text-secondary-font-color">
+      <h1 className=" text-3xl md:text-5xl font-semibold text-secondary-font-color drop-shadow-2xl">
         Message Queueing System
       </h1>
       <div className="pt-12 grid grid-cols-2 gap-x-8">
@@ -20,16 +20,17 @@ export default function Hero() {
         </Link>
       </div>
       <section className="py-8 text-xl md:text-3xl text-center font-semibold  text-transparent bg-clip-text bg-gradient-to-br from-[#AAAAAA] to-[#6B6B6B]">
-        <h2>Develop Message Queues Fast</h2>
-        <h2>In an easy, simple and distributed fashion.</h2>
+        <h2 className="drop-shadow-2xl">Develop Message Queues Fast</h2>
+        <h2 className="drop-shadow-2xl">
+          In an easy, simple and distributed fashion.
+        </h2>
       </section>
-      <section className=" relative w-full h-[480px]">
+      <section className=" relative w-full h-120">
         <Image
-          className="w-full"
+          className="w-full object-cover"
           src="/hero-cover.png"
           fill={true}
           alt="hero image"
-          objectFit="cover"
         />
       </section>
     </section>
